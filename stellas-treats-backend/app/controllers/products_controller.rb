6daf8f5c4ea563feb2f8913dc74d4d1324248c35
Nicole Dow <nikki.dow71@gmail.com>
2020-02-06 16:14:03 -0500

    new products can be posted to store

--- stellas-treats-backend/app/controllers/products_controller.rb
+++ stellas-treats-backend/app/controllers/products_controller.rb
@@ -1,12 +1,28 @@
 require 'rest-client'
 
 class ProductsController < ApplicationController
+
     def index
         #https://{apikey}:{password}@{hostname}/admin/api/{version}/{resource}.json
         products_response = RestClient.get("https://#{ENV['SHOPIFY_API_KEY']}:#{ENV['SHOPIFY_ADMIN_PASSWORD']}@stellas-treats.myshopify.com/admin/api/2020-01/products.json")
         products = JSON.parse(products_response)
 
         render json: products
+    end
+
+    def create
+        #binding.pry
+        new_product_hash = {
+            "product": {
+                "title": params["productTitle"],
+                "body_html": params["productDescripton"],
+                "tags": params["productTags"]
+            }
+        }
+        new_product = RestClient.post(
+            "https://#{ENV['SHOPIFY_API_KEY']}:#{ENV['SHOPIFY_ADMIN_PASSWORD']}@stellas-treats.myshopify.com/admin/api/2020-01/products.json",
+            new_product_hash
+        )
     end
 
 end
